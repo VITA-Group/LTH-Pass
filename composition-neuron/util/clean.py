73a44fdbfa6de9c38179237da0bd8cf086f4845e
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-12-06 14:55:09 +0800

    1

--- composition-neuron/util/clean.py
+++
@@ -1,8 +0,0 @@
-import settings
-import os
-
-
-def clean():
-    filelist = [f for f in os.listdir(settings.OUTPUT_FOLDER) if f.endswith("mmap")]
-    for f in filelist:
-        os.remove(os.path.join(settings.OUTPUT_FOLDER, f))
